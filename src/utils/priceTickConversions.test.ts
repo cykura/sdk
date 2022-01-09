3539334fb37dbc222fc8f05892ae09b2a74f8160
zkwarlock <shardul.aeer@somaiya.edu>
2022-01-09 13:25:50 +0530

    test: Update tests for price to tick calculations

--- src/utils/priceTickConversions.test.ts
+++ src/utils/priceTickConversions.test.ts
@@ -27,7 +27,6 @@ describe('priceTickConversions', () => {
 
   const token0 = token({ sortOrder: 0 })
   const token1 = token({ sortOrder: 1 })
-  const token2_6decimals = token({ sortOrder: 2, decimals: 6 })
 
   describe('#tickToPrice', () => {
     it('1800 t0/1 t1', () => {
@@ -45,32 +44,6 @@ describe('priceTickConversions', () => {
     it('1 t0/1800 t1', () => {
       expect(tickToPrice(token1, token0, 74959).toSignificant(5)).toEqual('0.00055556')
     })
-
-    describe('12 decimal difference', () => {
-      it('1.01 t2/1 t0', () => {
-        expect(tickToPrice(token0, token2_6decimals, -276225).toSignificant(5)).toEqual('1.01')
-      })
-
-      it('1 t0/1.01 t2', () => {
-        expect(tickToPrice(token2_6decimals, token0, -276225).toSignificant(5)).toEqual('0.99015')
-      })
-
-      it('1 t2/1.01 t0', () => {
-        expect(tickToPrice(token0, token2_6decimals, -276423).toSignificant(5)).toEqual('0.99015')
-      })
-
-      it('1.01 t0/1 t2', () => {
-        expect(tickToPrice(token2_6decimals, token0, -276423).toSignificant(5)).toEqual('1.0099')
-      })
-
-      it('1.01 t2/1 t0', () => {
-        expect(tickToPrice(token0, token2_6decimals, -276225).toSignificant(5)).toEqual('1.01')
-      })
-
-      it('1 t0/1.01 t2', () => {
-        expect(tickToPrice(token2_6decimals, token0, -276225).toSignificant(5)).toEqual('0.99015')
-      })
-    })
   })
 
   describe('#priceToClosestTick', () => {
@@ -82,12 +55,16 @@ describe('priceTickConversions', () => {
       expect(priceToClosestTick(new Price(token0, token1, 1800, 1))).toEqual(-74960)
     })
 
-    it('1.01 t2/1 t0', () => {
-      expect(priceToClosestTick(new Price(token0, token2_6decimals, 100e18, 101e6))).toEqual(-276225)
+    it('40 t1/1 t0', () => {
+      expect(priceToClosestTick(new Price(token0, token1, 1, 40))).toEqual(36890)
     })
 
-    it('1 t0/1.01 t2', () => {
-      expect(priceToClosestTick(new Price(token2_6decimals, token0, 101e6, 100e18))).toEqual(-276225)
+    it('50 t1/1 t0', () => {
+      expect(priceToClosestTick(new Price(token0, token1, 1, 50))).toEqual(39122)
+    })
+
+    it('60 t1/1 t0', () => {
+      expect(priceToClosestTick(new Price(token0, token1, 1, 60))).toEqual(40945)
     })
 
     describe('reciprocal with tickToPrice', () => {
@@ -105,14 +82,6 @@ describe('priceTickConversions', () => {
 
       it('1800 t1/1 t0', () => {
         expect(priceToClosestTick(tickToPrice(token0, token1, 74960))).toEqual(74960)
-      })
-
-      it('1.01 t2/1 t0', () => {
-        expect(priceToClosestTick(tickToPrice(token0, token2_6decimals, -276225))).toEqual(-276225)
-      })
-
-      it('1 t0/1.01 t2', () => {
-        expect(priceToClosestTick(tickToPrice(token2_6decimals, token0, -276225))).toEqual(-276225)
       })
     })
   })
